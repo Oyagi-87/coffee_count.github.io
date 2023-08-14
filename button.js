b2b094dd267c7c61823b6207ed92aab2d0415b0e
Ken-hnntsk <u020030@st.pu-toyama.ac.jp>
2023-08-14 15:26:50 +0900

    大元完成 あとは修正するとするとデザインのみかな

---
+++ button.js
@@ -0,0 +1,63 @@
+// 初期値
+let cups = 0;
+
+// ボタンと数値の要素を取得
+const minusOne = document.getElementById('minusOne');
+const minusHalf = document.getElementById('minusHalf');
+const plusTwo = document.getElementById('plusTwo');
+const plusFore = document.getElementById('plusFore');
+const plusSix = document.getElementById('plusSix');
+const plusEight = document.getElementById('plusEight');
+const plusTen = document.getElementById('plusTen');
+const clear = document.getElementById("clear");
+const beforeTime = document.getElementById("beforeTime");
+
+const numberElement = document.getElementById('counter');
+
+// -1ボタンのクリックイベント
+minusOne.addEventListener('click', decreaseNumber.bind(null, 1));
+//-0.5ボタンのクリックイベント
+minusHalf.addEventListener('click', decreaseNumber.bind(null, 0.5));
+
+// 加算ボタンのクリックイベント
+plusTwo.addEventListener('click', operateNumber.bind(null, 2));
+
+plusFore.addEventListener('click', operateNumber.bind(null, 4));
+plusSix.addEventListener('click', operateNumber.bind(null, 6));
+plusEight.addEventListener('click', operateNumber.bind(null, 8));
+plusTen.addEventListener('click', operateNumber.bind(null, 10));
+// clearボタンのクリックイベント
+clear.addEventListener('click',cupClear);
+
+// 数値の操作関数
+function operateNumber(value) {
+    const time = new Date();
+    cups += value;
+    updateNumberDisplay();
+    updateTime(time);
+}
+
+function decreaseNumber(value){
+    if(cups > 0.5){
+        cups = cups - value;
+        updateNumberDisplay();
+    }
+}
+
+function cupClear(){
+    cups = 0;
+    updateNumberDisplay();
+    beforeTime.innerHTML = "<h4>抽出されていません</h4>"
+}
+
+// 数値の表示を更新
+function updateNumberDisplay() {
+    numberElement.textContent = cups;
+}
+
+function updateTime(time){
+    const manth = time.getMonth()+1;
+    const day = time.getDate();
+    const sampletime = manth+"/"+day +" "+ time.toLocaleTimeString();
+    beforeTime.textContent = sampletime;
+}
